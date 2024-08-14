c2e49cf98a3bbcfde3225b08c28eb4a0436b83f3
Nuno André Ramos De Lemos <nuno@connecting-software.com>
2024-08-14 17:50:32 +0100

    Fixed error

--- components/pagination.tsx
+++ components/pagination.tsx
@@ -4,7 +4,7 @@ import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import Link from "next/link";
 import {page_routes} from "@/lib/routes-config";
 
-export default function Pagination({path, routes = page_routes, pathname }: { path: string, routes: { title: string; href: string; }[], pathname: string }) {
+export default function Pagination({path = "docs", routes = page_routes, pathname }: { path: string, routes: { title: string; href: string; }[], pathname: string }) {
   const res = getPreviousNext(routes, pathname);
 
   return (
@@ -13,7 +13,7 @@ export default function Pagination({path, routes = page_routes, pathname }: { pa
         {res.prev && (
           <Link
             className="flex items-center gap-2 no-underline text-sm px-1"
-            href={`/docs/${res.prev.href}`}
+            href={`/${path}/${res.prev.href}`}
           >
             <ChevronLeftIcon className="w-[1.1rem] h-[1.1rem]" />
             <p>{res.prev.title}</p>
